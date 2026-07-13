3d60a22ca0bba9df219c7fc653604d407e291838
Stratos888 <m_stoeckert@web.de>
2026-07-13 10:50:52 +0200

    Bridge backlog editing into existing source editor

---
+++ js/control-center-final-bridge.js
@@ -0,0 +1,12 @@
+(() => {
+  'use strict';
+
+  function bridge() {
+    document.querySelectorAll('[data-backlog-action="edit_source"]:not([data-labor-action])').forEach(button => {
+      button.dataset.laborAction = 'edit_source';
+    });
+  }
+
+  new MutationObserver(bridge).observe(document.body, { childList:true, subtree:true });
+  bridge();
+})();
